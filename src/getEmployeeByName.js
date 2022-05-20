d606177d2d395bf41d5e9201b8c00589830dc243
Arthur Araujo <technoasti@gmail.com>
2022-05-20 17:44:16 -0300

    Requisitos 2 e 3 concluidos + corringindo o Lint

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,8 +1,8 @@
 const { employees } = require('../data/zoo_data');
 
 const getEmployeeByName = (employeeName) => {
-  if (!employeeName) return {}
-  return employees.find((employee) => employee.firstName === employeeName || employee.lastName === employeeName);
-}
+  if (!employeeName) return {};
+  return employees.find((emp) => emp.firstName === employeeName || emp.lastName === employeeName);
+};
 
 module.exports = getEmployeeByName;
