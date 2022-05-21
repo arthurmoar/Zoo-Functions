bc5d64f4a1dfdaa6b9a59a39a751671470ed0857
Arthur Araujo <technoasti@gmail.com>
2022-05-20 22:25:23 -0300

    Requisito 5 concluido

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,23 @@
-const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
-function countAnimals(animal) {
-  // seu código aqui
+const getAnimals = () => {
+  const countBySpecie = {};
+
+  species.forEach(({ name, residents }) => {
+    countBySpecie[name] = residents.length;
+  });
+
+  return countBySpecie;
+}
+
+const countAnimals = (animal) => {
+  if (!animal) return getAnimals();
+
+  const { residents } = species.find(({ name }) => name === animal.specie);
+
+  const getByGender = residents.filter(({ sex }) => sex === animal.sex).length;
+
+  return (animal.sex) ? getByGender : residents.length;
 }
 
 module.exports = countAnimals;
