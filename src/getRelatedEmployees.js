5d21e291ff8e4926fac0a0deb14c8c888af1d03e
Arthur Araujo <technoasti@gmail.com>
2022-05-20 22:10:37 -0300

    Requisito 4 concluido

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,17 @@
-const data = require('../data/zoo_data');
+const { employees } = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  return employees.some(({ managers }) => managers.includes(id));
 }
 
-function getRelatedEmployees(managerId) {
-  // seu código aqui
+function getRelatedEmployees(gerenteID) {
+  if (isManager(gerenteID)) {
+    const gerentes = employees.filter(({ managers }) => managers.includes(gerenteID));
+
+    return gerentes.map(({ firstName, lastName }) => `${firstName} ${lastName}`);
+  }
+
+  throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 }
 
 module.exports = { isManager, getRelatedEmployees };
