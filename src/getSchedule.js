1b12ed6ef95d64fbb72b092ff215a2b8b3ede253
Arthur Araujo <technoasti@gmail.com>
2022-05-22 18:22:28 -0300

    Requisito 9 concluido

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,49 @@
-const data = require('../data/zoo_data');
+const { hours, species } = require('../data/zoo_data');
+
+const generateSchedule = () => {
+  const schedule = {};
+  Object.keys(hours).forEach((day) => {
+    const officeHour = hours[day];
+    const { open, close } = officeHour;
+    const getDay = species.filter(({ availability }) => availability.includes(day));
+    const namesDay = getDay.map(({ name }) => name);
+    const hoursDay = {};
+
+    hoursDay[day] = {
+      officeHour: `Open from ${open}am until ${close}pm`,
+      exhibition: namesDay,
+    };
+
+    Object.assign(schedule, hoursDay);
+  });
+
+  schedule.Monday = { officeHour: 'CLOSED', exhibition: 'The zoo will be closed!' };
+  return schedule;
+}
+
+const filterDay = ((scheduleTarget) => {
+  const scheduleOfDay = {};
+  const scheduleSelected = generateSchedule()[scheduleTarget];
+
+  scheduleOfDay[scheduleTarget] = scheduleSelected;
+
+  return scheduleOfDay;
+});
+
+const getAnimal = ((animal) => {
+  return species.find(({ name }) => name === animal).availability;
+});
 
 function getSchedule(scheduleTarget) {
-  // seu código aqui
+  const week = Object.keys(hours);
+  const animal = species.map(({ name }) => name);
+  const catWeekday = week.includes(scheduleTarget);
+  const catAnimal = animal.includes(scheduleTarget);
+
+  if (catWeekday) return filterDay(scheduleTarget);
+  if (catAnimal) return getAnimal(scheduleTarget);
+
+  return generateSchedule();
 }
 
 module.exports = getSchedule;
